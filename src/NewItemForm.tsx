7d3a203b10c0728ff62da150ce44b314d7491e60
Rodrigo Moura <rodrigo.moura@auravant.com>
2021-09-16 10:23:40 -0300

    [ADD] useFocus y ref para hacer el focus al input

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles";
+import { useFocus } from "./utils/useFocus";
 
 interface NewItemFormProps {
   onAdd(text: string): void;
 }
 
-export const NewItemForm = (props: NewItemFormProps) => {
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
-  const { onAdd } = props;
+  const inputRef = useFocus();
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
